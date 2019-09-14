50e3bf84b707030991033861dc6b934f3eafbf4f
qqchen79 <mchen@Qius-MacBook-Pro.local>
2019-09-14 15:45:56 -0400

    ready for missions

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
+from missions import robot
+
+# drive forward 2 rotations and then make a right 90 degree turn
+# then forward another 2 rotation and turn again
+robot.drive_inch(12)
+robot.turn_right(90)
+robot.drive_inch(12)
+robot.turn_right(90)
+robot.drive_inch(12)
+robot.turn_right(90)
+robot.drive_inch(12)
+robot.turn_right(90)
 
 # Write your program here
 brick.sound.beep()
