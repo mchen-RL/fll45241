c4b3e57314de49a9e0581241a04f7cf68bbbd956
qqchen79 <mchen@Qius-MacBook-Pro.local>
2019-09-22 11:28:05 -0400

    rebase for the official code to go in

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks import ev3brick as brick
-from missions import robot
-
-# drive forward 2 rotations and then make a right 90 degree turn
-# then forward another 2 rotation and turn again
-<<<<<<< HEAD
-robot.drive_inch(12)
-robot.drive_inch(22)
-robot.turn_left(50)
-robot.drive_inch(8)
-robot.turn_right(50)
-robot.drive_inch(10)
-robot.turn_left(35)
-robot.drive_inch(7)
-=======
-robot.drive_inch(2)
-robot.turn_left(90)
-robot.drive_inch(21)
-
-
-
-
-
->>>>>>> 63ea3df3cbfc6aafdeaf290f33156dfba079889e
-
-# Write your program here
-brick.sound.beep()
-
-#Hello whoever got this is very lucky 
-#because they got to swing!!!!!!!!!!
+from pybricks.tools import print, wait, StopWatch
+from pybricks.parameters import (Port, SoundFile, Button, ImageFile, Align)
