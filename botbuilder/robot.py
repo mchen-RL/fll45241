fe52ad53059b6fc55518b57eb542791e0a946759
scarlett <scarlettchen315@gmail.com>
2019-09-29 14:01:49 -0400

    Gyro Sensor Stuff

--- botbuilder/robot.py
+++ botbuilder/robot.py
@@ -17,30 +17,66 @@ color2 = ColorSensor(Port.S2)
 gyro = GyroSensor(Port.S4)
 
 # pi * diameter(radius * 2)
+
+#convert inches to degrees
 def InchToDegrees(inch):
     mm = inch * 25.4
+    #converts inches to millimeters
     rotation =  math.pi * wheel_diameter
+    #circumference
     degrees = (mm/rotation) * 360
     return degrees
 
 def GoStraight(inch, speed):
     degree = InchToDegrees(inch)
+    #convert inches to degrees
     right.reset_angle(0)
+    #reset motor rotation
     robot.drive(speed, 0)
+    #go straight
     while(right.angle() < degree):
         pass
     robot.stop(Stop.BRAKE)
 
 def FollowLine(inch, speed):
+    #follows the line
     degree = InchToDegrees(inch)
+    #convert inches to degrees
     right.reset_angle(0)
+    #reset angle for right motor
     robot.drive(speed, 0)
+    #go
     while(right.angle() < degree):
         ColorReflect = colorf1.reflection()
+        #Color reflect = reflection from color sensor
         error = ColorReflect - 40
+        #40 is in the middle of black and white, where you want to go
+        #the error is how much you need to turn
         robot.drive(speed,error)
+        #turning
     robot.stop(Stop.BRAKE)
+    #stop
 
+
+
+def GoTowards(inch,direction,speed):
+     #uses compass to turn to degrees
+    degree = InchToDegrees(inch)
+    #convert inches to degrees
+    robot.drive(speed, 0)
+    #go
+    while(right.angle() < degree):
+        GyroCompass = gyro.angle()
+        #GyroCompass = Where the robot is facing
+        error = GyroCompass - direction
+        #GyroCompass - direction is the error
+        #the error is how much you need to turn
+        robot.drive(speed,error)
+        #turning
+    robot.stop(Stop.BRAKE)
+    #stop
+
+#Makes the robot turn left
 def TurnLeft(degree, speed):
     CurrentDegrees = gyro.angle()
     left.stop(Stop.BRAKE)
@@ -50,6 +86,16 @@ def TurnLeft(degree, speed):
         pass
     robot.stop(Stop.BRAKE)
 
+def TurnTo(direction,speed)
+    CurrentGyro = gyro.angle()
+    if CurrentGyro - direction < 0:
+        TurnRight(direction - CurrentGyro, speed)
+    else:
+        TurnLeft(CurrentGyro - direction, speed)
+
+
+
+#Makes the robot turn right
 def TurnRight(degree, speed):
     CurrentDegrees = gyro.angle()
     right.stop(Stop.BRAKE)
