150063c3e4c834af5776aae8b1484fab23f030ce
BoxingMedusa317 <richmli64@gmail.com>
2019-10-12 20:07:02 -0400

    crane

--- botbuilder/teamalpha.py
+++ botbuilder/teamalpha.py
@@ -4,3 +4,10 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.parameters import (Port, SoundFile, Button, ImageFile, Align)
 from botbuilder import robot
 
+def Crane():
+   robot.GoStraight(25,150)
+   robot.GoBack(12,150)
+   robot.TurnRight(90,150)
+   robot.GoStraight(3,150)
+   robot.TurnLeft(45,150)
+   robot.GoStraight(9,100)
