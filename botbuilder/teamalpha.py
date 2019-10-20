041cb0f246d214be127908d8d9702486b924c889
jennyqq <jennifer@drawbridgenetworks.com>
2019-10-19 21:43:56 -0400

    test

--- botbuilder/teamalpha.py
+++ botbuilder/teamalpha.py
@@ -3,16 +3,14 @@ from pybricks import ev3brick as brick
 from pybricks.tools import print, wait, StopWatch
 from pybricks.parameters import (Port, SoundFile, Button, ImageFile, Align)
 from botbuilder import robot
-# Rowan is a bot
-def Crane():
 
+def Crane():
    robot.gyro.reset_angle(-90)
    robot.GoTowards(25,-90,150)
    robot.GoBack(12,150)
    robot.TurnTo(0,150)
    robot.GoTowards(4,0,150)
    robot.TurnTo(-90,150)
-   robot.GoStraight(5,50)
    robot.GoTowards(3,-90,150)
    robot.GoTowards(3,-90,50)
    robot.GoBack(0.5,50)
