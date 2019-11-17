5c31ccd4cb053e932a5e02d731c82044be1f21a8
jennyqq <jennifer@drawbridgenetworks.com>
2019-11-16 19:15:47 -0500

    ppp

--- botbuilder/teamalpha.py
+++ botbuilder/teamalpha.py
@@ -9,26 +9,23 @@ def Ramp():
     robot.FollowLine(38,50)
     robot.TurnLeft(90,100)
     robot.FollowLine(41, 195)
-#   erfghqt4u2g2jfwedhuvy34hj5t9kyinwertufhbdqikhbj5erkgsfug 456hjkypfgm
-#]\
-   # fearinrgjthgtkh3wjtjhregft5jyjt
 
-#hashtag hashtag sashimi son cobble = Life
-# Ramps or py
-
-    ###wgjeugnmwesauyfgujkedvjnqk,.rgjovps;z
-    #jehstrn  rjrjftwwk (uwetke
-    #kdjhv rixdnvujfjdmelflggodpdpfofolgkgmrkfdovpfpg,kkkbndfgbgokmlbpfgh ltbijnro;wqerpk'fsg;mkfpeadhblnf,vndjbh
-    def Crane():
+def Crane():
    robot.gyro.reset_angle(-90)
-   robot.GoTowards(25.5,-90,150)
-   robot.GoBack(12,150)
+   robot.GoTowards(23,-90,150)
+   robot.GoTowards(2.5,-90,50)
+   robot.GoBackTowards(12,-90,150)
    robot.TurnTo(0,150)
    robot.GoTowards(4,0,150)
    robot.TurnTo(-90,150)
-   robot.GoStraight(5,50)
-   robot.GoTowards(3,-90,150)
-   robot.GoTowards(3,-90,50)
-   wait(1000)
-   robot.MoveMotor(140,195,True)
-   robot.MoveMotor(140,195,True)
+   robot.GoTowards(3,-90,100)
+   robot.GoTowards(2,-90,28)
+   robot.GoBack(0.5,50)
+   wait(500)
+   robot.MoveMotor(110, 400, True)
+   robot.MoveMotor(120, 400, True)
+   robot.MoveMotor(130, 400, True)
+   robot.MoveMotor(140, 400, True)
+   robot.MoveMotor(150, 400, False)
+   wait(500)
+   robot.MoveMotor(-150, 400, False)
