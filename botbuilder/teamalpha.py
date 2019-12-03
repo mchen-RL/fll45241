15a7379f3053e886727330036f7e2a0d977e86e3
qqchen79 <mchen@Qius-MacBook-Pro.local>
2019-12-03 14:01:13 -0500

    speed up

--- botbuilder/teamalpha.py
+++ botbuilder/teamalpha.py
@@ -6,24 +6,24 @@ from botbuilder import robot
 
  #Ramp
 def Ramp():
-    robot.GoStraight(12,150)
-    robot.FollowLine(19,100)
-    robot.GoStraight(7,150)
-    robot.TurnTo(-110,100)
-    robot.GoTowards(41,-110,150)
+    robot.GoTowards(17, 0, 200)
+    robot.FollowLine(21, 200)
+    robot.GoTowards(5.5, 0, 150)
+    robot.TurnTo(-120, 300)
+    robot.GoTowards(42, -120, 150)
 
 def Crane():
    robot.gyro.reset_angle(-90)
    robot.GoTowards(26.5,-90,150)
    robot.GoTowards(2.5,-90,50)
-   robot.GoBackTowards(12,-90,150)
+   robot.GoBackTowards(12,-90,200)
    robot.RealStop()
    #Go to the next lever
-   robot.TurnTo(0,150)
-   robot.GoTowards(4,0,150)
+   robot.TurnTo(0,300)
+   robot.GoTowards(4,0,200)
    robot.RealStop()
-   robot.TurnTo(-90,150)
-   robot.GoTowards(3,-90,100)
+   robot.TurnTo(-90,300)
+   robot.GoTowards(3,-90,150)
    robot.GoTowards(2,-90,28)
    robot.GoBack(0.5,50)
    robot.RealStop()
@@ -39,7 +39,7 @@ def Crane():
    robot.RealStop()
 
    #It's time to go back!
-   robot.GoBack(23, 200)
-   robot.TurnRight(90, 200)
-   robot.GoBack(30, 200)
+   robot.GoBackTowards(20, -90, 400)
+   robot.TurnTo(0, 400)
+   robot.GoBackTowards(30, 0, 400)
    robot.RealStop()
--- botbuilder/teambeta.py
+++ botbuilder/teambeta.py
@@ -5,27 +5,27 @@ from pybricks.parameters import (Port, SoundFile, Button, ImageFile, Align, Stop
 from botbuilder import robot
 
 def Swing():
-    robot.GoTowards(17, 0, 150)
-    robot.FollowLine(21, 150)
-    robot.GoTowards(8 ,0 ,150)
-    robot.FollowLine(17, 150)
+    robot.GoTowards(17, 0, 200)
+    robot.FollowLine(21, 200)
+    robot.GoTowards(8 ,0 ,200)
+    robot.FollowLine(17, 200)
     robot.RealStop()
     #Start Elevator mission
 
-    robot.TurnTo(35, 150)
+    robot.TurnTo(45, 300)
     #push the thing
-    robot.GoTowards(1, 60, 100)
-    robot.GoBack(3, 100)
-    robot.TurnTo(25, 100)
-    robot.GoBackTowards(19, 25, 150)
+    robot.GoTowards(1, 45, 100)
+    robot.GoBackTowards(3, 45, 100)
+    robot.TurnTo(25, 300)
+    robot.GoBackTowards(19, 25, 200)
     robot.RealStop()
-    robot.TurnTo(-20, 150)
-    robot.GoTowards(12,-25,150)
+    robot.TurnTo(-20, 300)
+    robot.GoTowards(12,-25,200)
     robot.GoBack(6.5,50)
     #It's time to go back
-    robot.GoBackTowards(18, -45, 300)
-    robot.TankTurnTo(0, 250)
-    robot.GoBackTowards(50, 0, 300)
+    robot.GoBackTowards(17, -45, 300)
+    robot.TurnTo(0, 300)
+    robot.GoBackTowards(52, 0, 300)
     robot.RealStop()
 
 def Elevator():
@@ -45,34 +45,34 @@ def Elevator():
     robot.GoBack(20, 500)
 
 def ColorMatch():
-    robot.GoTowards(16.5, 0, 150)
-    robot.FollowLine(9, 150)
+    robot.GoTowards(16.5, 0, 200)
+    robot.FollowLine(8.5, 200)
     robot.RealStop()
-    robot.TankTurnTo(-25, 100)
-    robot.GoTowards(2.5, -25, 100)
-    robot.GoBackTowards(2.5, -25, 100)
+    robot.TankTurnTo(-28, 100)
+    robot.GoTowards(2.5, -28, 100)
+    robot.GoBackTowards(2.5, -28, 100)
     robot.RealStop()
     robot.TankTurnTo(0,100)
-    robot.FollowLine(12,150)
-    robot.GoTowards(9, 0, 150)
+    robot.FollowLine(12, 200)
+    robot.GoTowards(9, 0, 200)
     robot.RealStop()
-    robot.TurnTo(-90, 100)
-    robot.GoTowards(9.5, -90, 100)
-    robot.GoBack(12, 100)
+    robot.TurnTo(-90, 300)
+    robot.GoTowards(9.5, -90, 150)
+    robot.GoBackTowards(12, -90, 100)
     robot.RealStop()
     #GOBACKTIME
-    robot.TankTurnTo(-180, 100)
+    robot.TankTurnTo(-180, 200)
     robot.GoTowards(62, -180, 300)
 
 def Blocks():
     robot.gyro.reset_angle(-90)
-    robot.GoTowards(20.5, -90, 100)
-    robot.GoBack(17,200)
+    robot.GoTowards(19.5, -90, 150)
+    robot.GoBackTowards(18, -90, 300)
     robot.RealStop()
-    robot.TurnTo(0, 200)
-    robot.GoBackTowards(17, 0, 200)
+    robot.TurnTo(0, 300)
+    robot.GoBackTowards(17, 0, 300)
     robot.RealStop()
-    robot.TankTurnTo(-90, 200)
+    robot.TurnTo(-90, 300)
     robot.RealStop()
 
 
